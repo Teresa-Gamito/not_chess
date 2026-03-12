84ec193c0e827723e51bc7b7d92378f348351914
Hrodrick27 <168282166+Hrodrick27@users.noreply.github.com>
2026-03-12 16:28:44 +0000

    why

---
+++ include/game/board/piece.h
@@ -0,0 +1,23 @@
+#include <stdlib.h> 
+
+typedef struct Piece Piece;
+
+typedef enum PieceType {
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    QUEEN,
+    KING
+}PieceType;
+
+typedef enum PieceColour {
+    BLACK,
+    WHITE
+}PieceColour;
+
+Piece *piece_create (PieceType type, PieceColour colour);
+
+void piece_destroy (Piece *piece);
+
+void piece_set_type (Piece *piece, PieceType type);
---
+++ src/game/board/piece.c
@@ -0,0 +1,27 @@
+#include "../../../include/game/board/piece.h"
+
+struct Piece {
+    PieceType type;
+    PieceColour colour;
+};
+
+struct PieceProperties {
+
+};
+
+Piece *piece_create (PieceType type, PieceColour colour) {
+    Piece *piece = malloc(sizeof(Piece));
+    piece->type = type;
+    piece->colour = colour;
+    return piece;
+};
+
+void piece_destroy (Piece *piece) {
+    free(piece);
+};
+
+
+
+
+
+
